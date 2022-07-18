c060a372e8195e3bb62e51d47fb58c144eb3a8eb
Bryguy <bryansegarra044@gmail.com>
2022-07-18 18:26:16 -0400

    some codes

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,190 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+//   const Choices = require('inquirer/lib/objects/choices');
+// const { rejects } = require('assert');
+// const { resolve } = require('path');
 // TODO: Create an array of questions for user input
 const questions = [
    {
        type: 'input',
        name: 'title',
        message: 'What is the title of your project? (Required)',
-
+       validate: titleInput => {
+           if (titleInput){
+               return true;
+       } else {
+            console.log('please eneter your title!');
+            return false;
+       }
    }
+},
+{
+     type: 'input',
+     name: 'githubUsername',
+     message: 'What is your GitHub Username? (Required)',
+     validate: githubInput => {
+         if (githubInput){
+             return true;
+         } else {
+             console.log('Please enter your GitHub Username!');
+             return false;
+         }
+     }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your Email address? (Required)',
+    validate: githubInput => {
+        if (githubInput){
+            return true;
+        } else {
+            console.log('Please enter your email address!');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Enter your project description here: (Required)',
+    validate: descriptionInput =>{
+        if (descriptionInput) {
+            return true;
+        } else{
+            console.log('It is essential to provide a description of your project.!');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'why',
+    message: 'Why did you create this project? (Required)',
+    validate: whyinput => {
+        if (whyinput){
+        return true;
+    } else {
+        console.log('Please enter why you created this project!');
+        return false;
+       }
+    }
+},  
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide step-by-step installation instructions for your project. (Required)',
+    validate: installInput => {
+          if (installInput) {
+              return true;
+          } else {
+              console.log('Please enter your installation instructions!');
+              return false;
+          }
+    }
+},
+{
+    type: 'confirm',
+    name: 'confirmLicenses',
+    message: 'would you like to include a license? ',
+    default: false
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Which license will you use for your project?',
+    choices : ['agpl', 'apache', 'mit', 'no license'],
+    when: ({ confirmLicenses}) => {
+        if (confirmLicenses) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
+},
+{
+    type: 'confirm',
+    name: 'confirmContribute',
+    message: 'Would you like to allow other developers to contribute?',
+    default: true
+},
+{
+    type: 'input',
+    name: 'contribute',
+    message: 'Please provide guidelines for contributing. (Required)',
+    when: ({ cconfirmContributers }) => {
+        if (cconfirmContributers) {
+            return true;
+        } else {
+            return  false;
+        }
+    },
+    validate: contributerInput => {
+        if (contributerInput){
+            return true;
+        } else {
+            console.log ('Please enter contributer guidelines!');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'test',
+    message: 'Please provide instructions on how to test the app. (Required)',
+    validate: testInput => {
+        if (testInput) {
+            return true;
+        } else {
+            console.log('Please eneter your use test instructions!');
+            return false;
+        }
+     }
+}
 ];
 
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeToFile
+const writeToFile = data  => {
+    return new Promise((resolve, reject) => {
+        fs.writeToFile('./dist/generated-README.md', data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: console.Console.log('File created!')
+            });
+        })
+    })
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
 
 // Function call to initialize app
-init();
+init()
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeToFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err =>{
+    console.log(err);
+})
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,114 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderContributingSection(confirmContributers, data) {
+  if (!confirmContributers) {
+      return ` 
+      Thnak you for your interest in helping out; however, I will not be accepting contributions from third parties.
+      `;
+  } else {
+      return `
+      ${data}
+      `;
+  }
+}
+function renderLicenseBadge(license) { 
+  if (!license) {
+  return``;
+}else{
+  return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == 'MIT') {
+    return`![license](https://img.shields.io/badge/license-${license})`
+  }
+  if (license ==='Apache'){
+    return `[${License}(https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  if (license === 'CC--0') {
+    return `https://creativecommons.org/license/by-nd/4.0`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license'){
+    return `
+    ## [License](#table-of-contents)
+    
+    the application is covered under the following license:
+    
+    ${renderLicenseLink(license)}
+    `;
+  } else {
+    return '';
+  }
+}
+
+function renderLicenseTOC(license) {
+  if (license !== 'no license') {
+    return`
+    *[License](#license)
+    `;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table-of-Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseTOC(data.license)}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## [Description](#table-of-contents)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  
+  ##Description
+  ${data.description}
+
+  ## [Installation](#table-of-contents)
+  ${data.installation}
+
+  ## [Usage](#table-of-contents)
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.usage}
+  
+  ${renderContributingSection(data.confirmContributers, data.contribute)}
+  ## [Tests](#table-of-contents)
+  ${data.test}
+
+  ## [Questions](#table-of-contents)
+  Please contact me using the following links:
+  [GitHub](https://github.com/${data.githubUsername})
+  [Email: ${data.email}](mailto:${data.email})
+
+  ## Credits
+  ${data.name}
 `;
 }
 
